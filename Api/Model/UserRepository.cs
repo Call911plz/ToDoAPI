55dc5709895966a8eeee2eeeb73fb7bbc233664b
Hieu Truong <htruong6219@gmail.com>
2025-06-17 18:50:12 -0700

    feat: Add token generation to UserRepository

--- Api/Model/UserRepository.cs
+++ Api/Model/UserRepository.cs
@@ -6,6 +6,7 @@ public interface IUserRepository
     public List<User> GetUsers();
     public Task<User> UpdateUserAsync(User userToUpdate);
     public Task<bool> DeleteUserAsync(User userToDelete);
+    public string GenerateToken(int tokenLength);
 }
 
 public class UserRepository(ToDoDbContext context) : IUserRepository
@@ -44,5 +45,37 @@ public class UserRepository(ToDoDbContext context) : IUserRepository
         return await _context.Users
             .Where(user => user.Id == userToDelete.Id)
             .ExecuteDeleteAsync() > 0;
+    }
+
+    public string GenerateToken(int tokenLength)
+    {
+        string token = "";
+        Random rnd = new();
+
+        for (int i = 0; i < tokenLength; i++)
+        {
+            int character = rnd.Next(0, 62);
+            int startPos = 0;
+            switch (character)
+            {
+                case > 35:
+                    character -= 35;
+                    startPos = 97;
+                    break;
+                case > 9:
+                    character -= 9;
+                    startPos = 65;
+                    break;
+                default:
+                    startPos = 48;
+                    break;
+            }
+            token += Convert.ToChar(startPos + character);
+        }
+
+        if (token.All(char.IsLetterOrDigit) == false)
+            throw new Exception("Token generated non-alphanumeric letter");
+
+        return token;
     }
 }
--- Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adf614623bf0d3bc0e73ae41ff9d80eecc33e3f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49bfe55fbdb8c60aeed808b6b6b9f68850739618")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
